30fcfdd27095a47cfcade314eade75f43a3fb278
yasmeenattallah <yattallah7@gmail.com>
2021-05-28 21:04:50 +0300

    initalize component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from 'react';
 
 
 function App() {
   return (
     <div >
+   
   Hi from my portfolio
     </div>
   );
---
+++ src/component/header/index.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+
+const Header = ()=>{
+return (
+    <div> Hi from header </div>
+)
+}
+export default Header;
