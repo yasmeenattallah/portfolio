77554979cc90417784c671260fcfb4eed5045ee7
yasmeenattallah <yattallah7@gmail.com>
2021-05-30 19:14:18 +0300

    make  a switch with dark and light mode theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './component';
+import Dashboard from './theme';
 
 function App() {
   return (
     <div>
       <Header />
+      <Dashboard />
     </div>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,61 +1,64 @@
-import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import React, { useState } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
 
+export default function Dashboard() {
+  const [darkState, setDarkState] = useState(false);
+  const palletType = darkState ? 'dark' : 'light';
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+      text: {
+        primary: '#fffff',
+        secondary: '#C1C1C1',
+        disabled: '#989898',
+      },
+      background: {
+        primary: '#303030',
+        paper: '#424242',
+      },
+      divider: '#494949',
+      action: {
+        active: '#ffffff',
+        hover: '#404040',
+        selected: '#515151',
+        disabled: '#6F6F6F',
+        disabledBackground: '#303030',
+      },
+    },
+  });
+  const lightTheme = createMuiTheme({
+    palette: {
+      type: 'light',
+      text: {
+        primary: '#202020',
+        secondary: '#727272',
+        disabled: '#FAFAFA',
+      },
+      background: {
+        primary: '#FAFAFA',
+        paper: '#ffffff',
+      },
+      divider: '#DCDCDC',
+      action: {
+        active: '#727272',
+        hover: '#F0F0F0',
+        selected: '#E6E6E6',
+        disabled: '#BABABA',
+        disabledBackground: '#DCDCDC',
+      },
+    },
+  });
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
 
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    text: {
-      primary: '#fffff',
-      secondary: '#C1C1C1',
-      disabled: '#989898',
-    },
-    background: {
-      primary: '#303030',
-      paper: '#424242',
-    },
-    divider: '#494949',
-    action: {
-      active: '#ffffff',
-      hover: '#404040',
-      selected: '#515151',
-      disabled: '#6F6F6F',
-      disabledBackground: '#303030',
-    },
-  },
-});
-const lightTheme = createMuiTheme({
-  palette: {
-    type: 'light',
-    text: {
-      primary: '#202020',
-      secondary: '#727272',
-      disabled: '#FAFAFA',
-    },
-    background: {
-      primary: '#FAFAFA',
-      paper: '#ffffff',
-    },
-    divider: '#DCDCDC',
-    action: {
-      active: '#727272',
-      hover: '#F0F0F0',
-      selected: '#E6E6E6',
-      disabled: '#BABABA',
-      disabledBackground: '#DCDCDC',
-    },
-  },
-});
-const Theme = (props) => {
-  const { children, darkMode } = props;
-  const defaultTheme = darkMode ? darkTheme : lightTheme;
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={darkState ? darkTheme : lightTheme}>
       <CssBaseline />
-      {children}
+      <div> Sample Text </div>
+      <Switch checked={darkState} onChange={handleThemeChange} />
     </ThemeProvider>
   );
-};
-
+}
