98eee2deb44a06d0f48443e1e1bd1a5e8b76e8eb
yasmeenattallah <yattallah7@gmail.com>
2021-06-02 20:47:58 +0300

    Home page Fix #8

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,14 +3,13 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 import useStyles from './style';
-// import home from '../../images/home.jpg';
 
 const Home = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <div className={classes.background} />
-      <Typography>
+      <Typography className={classes.typo}>
         YASMEEN ATTALLAH <br />
         I&apos;M A FRONT-END WEB DEVELOPER
       </Typography>
