0bdd6c13c030f93d7210c1f4c0d203fc867426f8
yasmeenattallah <yattallah7@gmail.com>
2021-06-04 14:46:13 +0300

    About me page

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import useStyles from './style';
 
 const AboutMe = () => {
   const classes = useStyles();
-  return <div className={classes.root}> Hi from about me page</div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <Typography className={classes.title}>
+          About Me
+          <PersonOutlineOutlinedIcon className={classes.icon} />
+        </Typography>
+      </div>
+    </div>
+  );
 };
 export default AboutMe;
