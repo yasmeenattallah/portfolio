cf9f28d6bc5c592e21abc2f84992bba57a4cf4f4
yasmeenattallah <yattallah7@gmail.com>
2021-06-06 23:09:01 +0300

    remove commented line in theme file Fix #61

--- src/App/theme.js
+++ src/App/theme.js
@@ -58,49 +58,4 @@ const lightTheme = createMuiTheme({
   },
 });
 
-// const paletteType = darkState ? 'dark' : 'light';
-// const primaryText = darkState ? '#ffffff' : '#202020';
-// const secText = darkState ? '#C1C1C1' : '#727272';
-// const disableText = darkState ? '#989898' : '#FAFAFA';
-// const primaryBackground = darkState ? '#303030' : '#FAFAFA';
-// const paperGround = darkState ? '#424242' : '#727272';
-// const divider = darkState ? '#424242' : '#2a2b30';
-// const activeAction = darkState ? '#ffffff' : '#727272';
-// const hoverAction = darkState ? '#404040' : '#F0F0F0';
-// const selectedAction = darkState ? '#515151' : '#E6E6E6';
-// const disableAction = darkState ? '#6F6F6F' : '#BABABA';
-// const disableGroundAction = darkState ? '#303030' : '#DCDCDC';
-// const colorTextField = darkState ? '#fff' : '#000';
-
-// const darkTheme = createMuiTheme({
-//   palette: {
-//     type: paletteType,
-//     text: {
-//       primary: primaryText,
-//       secondary: secText,
-//       disabled: disableText,
-//     },
-//     background: {
-//       primary: primaryBackground,
-//       paper: paperGround,
-//     },
-//     divider,
-//     action: {
-//       active: activeAction,
-//       hover: hoverAction,
-//       selected: selectedAction,
-//       disabled: disableAction,
-//       disabledBackground: disableGroundAction,
-//     },
-//   },
-
-//   props: {
-//     MuiButton: {
-//       variant: 'outlined',
-//     },
-//     MuiTextField: {
-//       color: colorTextField,
-//     },
-//   },
-// });
 export { darkTheme, lightTheme };
