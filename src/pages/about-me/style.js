718dc8a281a5877d23711050c3d9638241bfd335
yasmeenattallah <yattallah7@gmail.com>
2021-06-07 10:57:57 +0300

    change light theme divider to be #727272 from theme file

--- src/pages/about-me/style.js
+++ src/pages/about-me/style.js
@@ -1,5 +1,33 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 1166,
+    height: 622,
+  },
+  header: {
+    borderBottom: `1px dotted ${theme.palette.divider}`,
+    width: 800,
+    display: 'inline-flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    '& .MuiTypography-body1': {
+      fontFamily: 'Kaushan Script',
+    },
+    margin: theme.spacing(6, 7),
+  },
+  title: {
+    color: '#00A3E1',
+    letterSpacing: 1.5,
+    fontSize: '2rem',
+  },
+
+  icon: {
+    color: '#00A3E1',
+    fontSize: '2rem',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+}));
 
 export default useStyles;
