e9ed63d35b70a2ae41afbbdffcf68bf4edfd819d
yasmeenattallah <yattallah7@gmail.com>
2021-06-09 18:46:07 +0300

    header section

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+import CardTravelOutlinedIcon from '@material-ui/icons/CardTravelOutlined';
 import useStyles from './style';
 
 const PortfolioPage = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <p> Hi from Portfolio page</p>
+      <div className={classes.header}>
+        <Typography className={classes.title}>Portfolio .</Typography>
+        <CardTravelOutlinedIcon className={classes.icon} />
+      </div>
     </div>
   );
 };
--- src/pages/portfolio/style.js
+++ src/pages/portfolio/style.js
@@ -2,7 +2,30 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: theme.palette.background.primary,
+    width: 1150,
+    height: 550,
+  },
+  header: {
+    borderBottom: `1px dotted ${theme.palette.divider}`,
+    width: 1000,
+    '& .MuiTypography-body1': {
+      display: 'inline-flex',
+      justifyContent: 'flex-start',
+      fontFamily: 'Kaushan Script',
+    },
+    margin: theme.spacing(6, 10),
+  },
+  title: {
+    color: '#00A3E1',
+    letterSpacing: 1.5,
+    fontSize: '2rem',
+    width: 900,
+  },
+
+  icon: {
+    color: '#00A3E1',
+    fontSize: '2.8rem',
+    marginBottom: '-13px',
   },
 }));
 export default useStyles;
