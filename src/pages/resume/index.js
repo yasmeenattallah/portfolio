e1049a78a34d2ae0873a96819a714abb8a171a80
yasmeenattallah <yattallah7@gmail.com>
2021-06-12 20:02:15 +0300

    resume body and style timeline Fix #21

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+
 import useStyles from './style';
 
 const ResumePage = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <p> Hi form resume</p>
+      <div className={classes.header}>
+        <Typography className={classes.title}>Resume .</Typography>
+        <ListAltOutlinedIcon className={classes.icon} />
+      </div>
+      <div className={classes.body}>
+        <Timeline align="right" className={classes.timeline}>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot className={classes.timelineDot} />
+              <TimelineConnector className={classes.timelineDot} />
+            </TimelineSeparator>
+            <TimelineContent className={classes.timelineContent}>
+              2015
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot className={classes.timelineDot} />
+              <TimelineConnector className={classes.timelineDot} />
+            </TimelineSeparator>
+            <TimelineContent className={classes.timelineContent}>
+              2020
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+        <Typography className={classes.sectionOne}>
+          <span className={classes.bodyTitle}> Eduction</span> <br />
+          computer and communication Engineering
+        </Typography>
+        <Typography className={classes.sectionTwo}>
+          <span className={classes.bodyTitle}> Experience </span>
+        </Typography>
+      </div>
     </div>
   );
 };
--- src/pages/resume/style.js
+++ src/pages/resume/style.js
@@ -1,9 +1,67 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: 1166,
-    height: 622,
+    width: 1100,
+    height: 570,
+  },
+  header: {
+    borderBottom: `1px dotted ${theme.palette.divider}`,
+    width: 1000,
+    '& .MuiTypography-body1': {
+      display: 'inline-flex',
+      justifyContent: 'flex-start',
+      fontFamily: 'Kaushan Script',
+    },
+    margin: theme.spacing(6, 10),
+  },
+  title: {
+    color: '#00A3E1',
+    letterSpacing: 1.5,
+    fontSize: '2rem',
+    width: 900,
+  },
+
+  icon: {
+    color: '#00A3E1',
+    fontSize: '2.8rem',
+    marginBottom: '-13px',
+  },
+  body: {
+    width: '1000px',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    margin: theme.spacing(0, 20),
+    color: theme.props.MuiTypography.color,
+  },
+  sectionOne: {
+    fontFamily: 'Arial',
+    width: '50%',
+    lineHeight: 2.178,
+  },
+  sectionTwo: {
+    width: '50%',
+    fontFamily: 'Arial',
+    lineHeight: 2.5,
+  },
+  bodyTitle: {
+    fontSize: '1.6rem',
+    letterSpacing: 1,
+    textTransform: 'uppercase',
+    position: 'relative',
+    fontFamily: 'Kaushan Script',
+    textDecoration: 'underline #00A3E1  2px',
+  },
+  timeline: {
+    marginLeft: '-80px',
+    marginTop: '50px',
+  },
+  timelineContent: {
+    color: '#00A3E1',
+    fontWeight: 'bold',
+  },
+  timelineDot: {
+    backgroundColor: '#00A3E1',
   },
 }));
 export default useStyles;
