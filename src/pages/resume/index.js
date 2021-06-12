e25356d8a3c8b4886cede5ed4c31c9a2eaaca2c0
yasmeenattallah <yattallah7@gmail.com>
2021-06-12 19:01:56 +0300

    Resume Page Fix #21

---
+++ src/pages/resume/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import useStyles from './style';
+
+const ResumePage = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <p> Hi form resume</p>
+    </div>
+  );
+};
+export default ResumePage;
