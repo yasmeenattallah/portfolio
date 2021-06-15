4b6e31b183ad3899b44ba2a0df2262d57526f185
yasmeenattallah <yattallah7@gmail.com>
2021-06-16 00:33:06 +0300

    fix header to be consistant with other pages Fix #25

--- src/pages/contact/style.js
+++ src/pages/contact/style.js
@@ -7,26 +7,25 @@ const useStyles = makeStyles((theme) => ({
   },
   header: {
     borderBottom: `1px dotted ${theme.palette.divider}`,
-    width: 800,
-    display: 'inline-flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    width: 1000,
     '& .MuiTypography-body1': {
+      display: 'inline-flex',
+      justifyContent: 'flex-start',
       fontFamily: 'Kaushan Script',
     },
-    margin: theme.spacing(6, 7),
+    margin: theme.spacing(6, 10),
   },
   title: {
     color: '#00A3E1',
     letterSpacing: 1.5,
     fontSize: '2rem',
+    width: 900,
   },
 
   icon: {
     color: '#00A3E1',
-    fontSize: '2rem',
-    display: 'flex',
-    justifyContent: 'flex-end',
+    fontSize: '2.8rem',
+    marginBottom: '-13px',
   },
   subTitle: {
     fontWeight: 'bold',
