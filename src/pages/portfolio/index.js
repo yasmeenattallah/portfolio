3eaf3f6c09c341a762152c4913d8a91badceab01
yasmeenattallah <yattallah7@gmail.com>
2021-06-23 12:29:15 +0300

    edit portfolio style

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -6,8 +6,6 @@ import Link from '@material-ui/core/Link';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
 
 import useStyles from './style';
 import beautySalon from '../../images/beautySalon.png';
@@ -21,53 +19,31 @@ const PortfolioPage = () => {
         <Typography className={classes.title}>Portfolio .</Typography>
         <CardTravelOutlinedIcon className={classes.icon} />
       </div>
-      <div className={classes.body}>
-        <GridList className={classes.linkImg}>
-          <GridListTile>
-            <Link
-              href="https://github.com/yasmeenattallah/house-hunting-app"
-              component="a"
-              target="_blank"
-              className={classes.linkImg}
-            >
-              <img
-                src={houseHunting}
-                alt="HouseHunting"
-                className={classes.img}
-              />
-            </Link>
-            <GridListTileBar
-              title="House Hunting"
-              actionIcon={
-                <IconButton aria-label="info about" className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
+      <GridList>
+        <GridListTile className={classes.body}>
+          <Link
+            href="https://github.com/yasmeenattallah/house-hunting-app"
+            component="a"
+            target="_blank"
+          >
+            <img
+              src={houseHunting}
+              alt="HouseHunting"
+              className={classes.img}
             />
-          </GridListTile>
-          <GridListTile>
-            <Link
-              href="https://github.com/yasmeenattallah/beauty-salon"
-              target="_blank"
-              className={classes.linkImg}
-            >
-              <img
-                src={beautySalon}
-                alt="BeautySalon"
-                className={classes.img}
-              />
-            </Link>
-            <GridListTileBar
-              title="Beauty Salon"
-              actionIcon={
-                <IconButton aria-label="info about" className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        </GridList>
-      </div>
+          </Link>
+          <GridListTileBar title="House Hunting" />
+        </GridListTile>
+        <GridListTile className={classes.body}>
+          <Link
+            href="https://github.com/yasmeenattallah/beauty-salon"
+            target="_blank"
+          >
+            <img src={beautySalon} alt="BeautySalon" className={classes.img} />
+          </Link>
+          <GridListTileBar title="Beauty Salon" />
+        </GridListTile>
+      </GridList>
     </div>
   );
 };
