0445911bc961c73eff9d766382df746ef2fde313
yasmeenattallah <yattallah7@gmail.com>
2021-07-10 00:19:58 +0300

    body responsive

--- src/pages/portfolio/style.js
+++ src/pages/portfolio/style.js
@@ -56,10 +56,11 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
     },
   },
-  '& .MuiGridListTile-imgFullWidth': {
-    border: '5px solid #f00',
+  link: {
+    width: '100%',
+    height: '100%',
+    border: '2px solid #f0f',
   },
-
   img: {
     objectFit: 'cover',
     width: '100%',
